be4d65730702bf53af30d395eafea8785e92f9e5
Alvaro Barroso <abarroso@salesforce.com>
2019-05-12 23:14:19 +0200

    no message

--- Controller.py
+++ Controller.py
@@ -17,13 +17,9 @@ class ArbolDeDecision(Resource):
         return {'clase': []}  # Fetches first column that is Employee ID
 
 
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d " % int(employee_id))
-        result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
-        return jsonify(result)
-
 
 api.add_resource(Clasificador, '/clasificador')  # Route_1
 api.add_resource(ArbolDeDecision, '/arboldedecision')  # Route_1
+
+if __name__ == "__main__":
+  app.run()
