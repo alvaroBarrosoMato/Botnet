9b3f160f0c39cd3c5e3e33219b87bdc6e8e3ecf2
Alvaro Barroso <abarroso@salesforce.com>
2019-05-13 19:16:55 +0200

    no message

--- Controller.py
+++ Controller.py
@@ -1,37 +1,36 @@
 from flask import Flask
 from DecisionTree import DecisionTreeClass
 
-myApp = Flask(__name__)
+app = Flask(__name__)
 dtree = DecisionTreeClass()
 
 
-@myApp.route("/")
+@app.route("/")
 def index():
     return "Index!"
 
-@myApp.route("/build")
+@app.route("/build")
 def build():
     dtree.build("dataset2.csv")
     time = dtree.getBuildTime()
     return "time = " + str(time)
 
-@myApp.route("/load")
+@app.route("/load")
 def load():
     dtree.loadtree()
     return 'loaded'
 
-@myApp.route("/time")
+@app.route("/time")
 def time():
     time = dtree.getBuildTime()
     return "time = " + str(time)
 
-@myApp.route("/members")
+@app.route("/members")
 def members():
     return "Members"
 
 
 
-if myApp == "__main__":
-    myApp.run()
-
+if __name__ == "__main__":
+    app.run()
 
