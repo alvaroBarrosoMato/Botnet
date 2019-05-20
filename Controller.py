d44a4c3f1d227d3160e66ce125e75d8cb36e269f
Alvaro Barroso <abarroso@salesforce.com>
2019-05-20 19:21:23 +0200

    no message

--- Controller.py
+++ Controller.py
@@ -10,38 +10,22 @@ app = Flask(__name__)
 dtree = DecisionTreeClass()
 
 
-dTreeQueue = Queue('dTree', is_async=False, connection=conn)
+dTreeQueue = Queue('dTree', connection=conn)
 
-jobList = []
-treeList = []
+jobList = {}
+treeList = {}
 workersLimit = 10
 
 
-@app.route("/dtree/test/<int:index>")
-def test(index):
-    print("treeList: "+ str(len(treeList)))
-    if len(treeList) >= index:
-        tree = treeList[index]
-        treeList[index] = dTreeQueue.enqueue(tree.test, "testDataset.csv")
-        return "testing"
-    else:
-        return "Index Out of Bounds"
-
 @app.route("/dtree/size")
 def testing():
-    print("tree - " + str(len(treeList)))
-    print("job - " + str(len(jobList)))
+    queued_job_ids = dTreeQueue.job_ids
+    queued_jobs = dTreeQueue.jobs
+    for ids in queued_job_ids:
+        print(ids)
+    for jobs in queued_jobs:
+            print(jobs.result)
     return "done"
-
-
-@app.route("/dtree/getTime/<int:index>")
-def getTime(index):
-
-    while len(treeList) > 0:
-        
-    print("Time = " + str(treeList[index].buildTime))
-    return str(treeList[index].buildTime)
-
 
 @app.route("/dtree/traintest")
 def buildAllMix():
@@ -49,37 +33,9 @@ def buildAllMix():
     i = 0
     while i < workersLimit:
         trainJob = dTreeQueue.enqueue(construir, "mix.csv", i)
-        jobList.append(trainJob)
         i = i + 1
     print("Hola")
     return "training"
-
-@app.route("/dtree/traintest2")
-def buildAllMix2():
-    dtreeStatus = 'training'
-    i = 0
-    while i < workersLimit:
-
-        jobList.append(dTreeQueue.enqueue(construir, "mix.csv", i))
-        i = i + 1
-    print("Hola")
-    return "training"
-
-@app.route("/dtree/load")
-def load():
-    print(len(jobList))
-    if len(jobList) > 0:
-        if len(treeList) == workersLimit:
-            return "Algorithms Built and Loaded"
-        else:
-            i = 0
-            while i < len(jobList):
-                treeList.append(jobList[i].result)
-                i = i + 1
-            dtreeStatus = 'loaded'
-            print(len(treeList))
-            return "loaded"
-    return "Waiting for Algorithms to Build"
 
 if __name__ == "__main__":
     app.run()
